2f035f6d4fe26ccbb3581079d6a77ea59742aef4
Ted Dixon <ted@semperdynamics.com>
2018-02-11 20:17:50 -0500

    Branch 1 commit with changes to add new class and method.

---
+++ WhiskeyBAL/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiskeyBAL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// GetInt
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int GetInt(int input)
+        {
+            return input + 666999;
+        }
+    }
+}
